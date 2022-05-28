4a71972343a8ffbd781dcfdc70234ab36b3bbf5f
elmiy <meyuanaa@gmail.com>
2022-05-29 04:08:17 +0700

    add everything

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <script>
+        tailwind.config = {
+          theme: {
+            colors: {
+                main: '#3C4599',
+                sec: '#FAEFB1',
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'] 
+            },
+          }
+        }
+      </script>
+    <title>The Spaces</title>
+</head>
+<body class="font-poppins">
+    <div class="bg-main h-24">
+        <div class="flex justify-end">
+            <div class="flex text-2xl py-7 mx-1 font-medium text-[color:white]">
+                <a href="home.php" class="px-4">Home</a>
+                <a href="#" class="px-4">Our Spaces</a>
+                <a href="#" class="px-4">Articles</a>
+            </div>
+            <div class="flex text-2xl py-5 mx-4 mr-8 text-main">
+                <button class="bg-sec px-6 py-1 rounded-3xl">Login</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- the_spaces.sql
+++ the_spaces.sql
@@ -1,22 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 28, 2022 at 06:18 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `the_spaces`
+--
+
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE `artikel` (
+  `id_artikel` varchar(10) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `artikel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_service`
+--
+
+CREATE TABLE `customer_service` (
+  `id_input` varchar(10) NOT NULL,
+  `input` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `tipe_durasi` varchar(15) NOT NULL,
+  `durasi` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bukti_pembayaran` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spaces`
+--
+
+CREATE TABLE `spaces` (
+  `id_spaces` varchar(10) NOT NULL,
+  `nama_spaces` varchar(15) NOT NULL,
+  `tipe_spaces` varchar(15) NOT NULL,
+  `harga_jam` int(11) DEFAULT NULL,
+  `harga_hari` int(11) DEFAULT NULL,
+  `harga_minggu` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `id_user` varchar(10) NOT NULL,
+  `id_spaces` varchar(10) NOT NULL,
+  `nama_lengkap` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `tipe_durasi` varchar(15) NOT NULL,
+  `durasi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
   `id_user` varchar(10) NOT NULL,
-  `nama_lengkap` varchar(45) NOT NULL,
+  `nama_user` varchar(45) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `domisili` varchar(15) NOT NULL,
   `email` varchar(45) NOT NULL,
   `no_telp` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voucher`
+--
+
+CREATE TABLE `voucher` (
+  `id_voucher` varchar(10) NOT NULL,
+  `voucher` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id_artikel`);
+
+--
+-- Indexes for table `customer_service`
+--
+ALTER TABLE `customer_service`
+  ADD PRIMARY KEY (`id_input`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- Indexes for table `spaces`
+--
+ALTER TABLE `spaces`
+  ADD PRIMARY KEY (`id_spaces`),
+  ADD UNIQUE KEY `id_spaces` (`id_spaces`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD UNIQUE KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_spaces` (`id_spaces`);
+
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `voucher`
+--
+ALTER TABLE `voucher`
+  ADD PRIMARY KEY (`id_voucher`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_spaces`) REFERENCES `spaces` (`id_spaces`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_transaksi`) REFERENCES `pembayaran` (`id_transaksi`);
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
