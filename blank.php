3e146e4da3cb3ed7e68f4071683bd0bc0f61f4c6
Retno Ekayanti <93781028+rahayurdn@users.noreply.github.com>
2022-06-02 11:45:43 +0700

    add blank page

---
+++ blank.php
@@ -0,0 +1,48 @@
+<?php
+include 'navbar.php';
+?>
+<main>
+        <article class="container-fluid">
+            <section class="mx-3">
+                <div class="headlines">
+                    <h1>ARTICLE</h1>
+                    <img src="assets/pic/img-headlines.jpg" alt="img-headlines">
+                </div>
+            </section>
+            <section class="row align-items-center mx-2 mt-5 mb-4">
+                <div class="col-9">
+                    <h2 class="text-secondary">Artikel The Spaces</h2>
+                    <h4 class="text-secondary h5 font-weight-light">Dapatkan info terbaru terkait workspace disini!</h4>
+                </div>
+                <div class="col-3">
+                    <div class="d-flex align-items-center">
+                        <button class="btn">
+                            <img src="assets/pic/search.jpg" alt="search-icon" width="25">
+                        </button>
+                        <div class="input-group">
+                            <input type="text" class="form-control rounded-pill" placeholder="Workspace Jakarta"
+                                aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="d-flex justify-content-center align-items-center mb-4" style="height: 400px;">
+                <div class="text-black-50 font-weight-light" style="font-size: 50px;">
+                    Artikel Tidak di Temukan
+                </div>
+            </section>
+        </article>
+    </main>
+    <footer>
+        <span class="d-flex justify-content-center pb-4 text-black-50">Menampilkan 1 dari 1 Halaman</span>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
