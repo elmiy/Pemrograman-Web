b4b2ae84b27088675009140b71c27f6197aa5229
Retno Ekayanti <93781028+rahayurdn@users.noreply.github.com>
2022-06-02 11:44:24 +0700

    Add article

---
+++ article2.php
@@ -0,0 +1,68 @@
+<?php
+include 'navbar.php';
+?>
+ <main>
+        <article class="container-fluid">
+            <section class="row align-items-center mx-3" style="padding-top: 35px;">
+                <div class="col-9">
+                    <a class="btn" href="article.html">
+                        <i class="fa fa-angle-left" style="font-size: 18px; margin-right: 8px;"></i>
+                        <span class="text-secondary font-weight-normal" style="font-size: 19px;">
+                            Kembali ke Artikel
+                        </span>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <div class="d-flex align-items-center">
+                        <button class="btn">
+                            <img src="assets/pic/search.jpg" alt="search-icon" width="25">
+                        </button>
+                        <div class="input-group">
+                            <input type="text" class="form-control rounded-pill" placeholder="search you want"
+                                aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="mx-3">
+                <div class="headlines">
+                    <img src="assets/pic/card-2.jpg" alt="img-card" style="max-height: 560px; border-radius: 60px;">
+                </div>
+            </section>
+            <section class="my-5 mx-3">
+                <h1 style="font-size: 50px; color: rgba(60, 69, 153, 0.73);">
+                    4 Tips Membangun Networking di Coworking Space
+                </h1>
+                <h2 class="h5 text-secondary font-weight-light">Oleh : confieid</h2>
+                <div class="wrap-description mt-4">
+                    <p>
+                        Salah satu manfaat penting dari coworking space adalah networking. Bayangkan Anda berada di
+                        tengah-tengah orang dengan passion yang sama, pengalaman yang berbeda-beda, dan tentunya mereka
+                        semua mempunyai peluang kerja sama yang sangat besar. Tetapi sebelum Anda melakukan networking
+                        di coworking space, 4 Tips penting ini yang harus Kalian perhatikan!
+                    </p>
+                    <ol>
+                        <li>Berbicara Mengenai Bisnis dan Pengalaman</li>
+                        <li>Menawarkan sesuai kebutuhan</li>
+                        <li>Share Pengalaman</li>
+                        <li>Tunjukkan Passion</li>
+                    </ol>
+                    <p>
+                        Lalu, apa efeknya? Anda akan menjadi lawan bicara yang sangat menarik dan mudah diingat. Mungkin
+                        lawan bicara Anda tersebut tidak membutuhkan kerja sama untuk sekarang, tetapi untuk masa depan
+                        siapa yang tahu?
+                    </p>
+                </div>
+            </section>
+        </article>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
